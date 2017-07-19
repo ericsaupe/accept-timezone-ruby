3fbad2cad34c116fa721adc06ddeb8cf565620bc
Eric Saupe <ericsaupe@gmail.com>
2017-07-19 14:41:34 -0600

    Move middleware further down the stack

--- lib/accept_timezone/railtie.rb
+++ lib/accept_timezone/railtie.rb
@@ -1,7 +1,7 @@
 module AcceptTimezone
   class Railtie < Rails::Railtie
     initializer "accept_timezone.configure_rails_initialization" do
-      Rails.application.middleware.insert_before Rack::Sendfile, AcceptTimezone::Middleware
+      Rails.application.middleware.use AcceptTimezone::Middleware
     end
   end
 end
